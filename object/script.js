71f1bbc1755ae5f3c07ace28310a2a54826d480b
Thai Le Q <thai.le@asiantech.vn>
2018-06-28 15:33:32 +0700

    ver 2

---
+++ object/script.js
@@ -0,0 +1,19 @@
+var product1 = { name: 'Samsung galaxy note 7', price: '600',image: 'anh1.jpg' };
+var product2 = { name: 'Iphone 8', price: '1000',image: 'anh2.jpg'};
+var product3 = { name: 'Laptop Dell inspiron', price: '560',image: 'anh3.jpg' };
+var product4 = { name: 'Laptop Dell inspiron', price: '560',image: 'anh4.jpg' };
+var product5 = { name: 'Laptop Asus', price: '560',image: 'anh5.jpg' };
+var product6 = { name: 'Nokia 1280', price: '60',image: 'anh6.jpg' };
+var product7 = { name: 'Laptop Dell inspiron', price: '560',image: 'anh7.jpg' };
+var product8 = { name: 'Zenphone 5', price: '400',image: 'anh8.jpg' };
+
+var listproduct = [];
+listproduct.push(product1); 
+listproduct.push(product2);
+listproduct.push(product3);
+listproduct.push(product4);
+listproduct.push(product5);
+listproduct.push(product6);
+listproduct.push(product7);
+listproduct.push(product8);
+console.log(listproduct);
